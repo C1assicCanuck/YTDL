9868c0f870653a7a62f99cebba833f7b6448551f
C1assicCanuck <44477407+C1assicCanuck@users.noreply.github.com>
2021-12-10 00:57:19 +0600

    Add files via upload

--- YTDL.py
+++ YTDL.py
@@ -1,22 +1,40 @@
-import youtube_dl, ffmpeg, os, sys
+import yt_dlp as youtube_dl
+import ffmpeg, os, sys, threading
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QIcon
 from PyQt6 import uic
+from PyQt6.QtCore import pyqtSignal
 
 wdir = str(os.path.dirname(__file__) + "\\")
 os.chdir(wdir)
 outpath = str(os.environ["USERPROFILE"] + "\\Downloads\\")
 
+illegalc = ("""\/:*?"<>|""")
+def remove(value):
+    for c in illegalc:
+        value = value.replace(c,"")
+    return value
+
 class app(QWidget):
+    text = pyqtSignal(str)
+    progress = pyqtSignal(int)
+
     def __init__(self):
         super().__init__()
         uic.loadUi("ui.ui", self)
         self.setWindowIcon(QIcon('ico.ico'))
-        self.ok.clicked.connect(self.getdata)
+        self.ok.clicked.connect(self.startdl)
+        self.progress.connect(self.progressBar.setValue)
+        self.text.connect(self.information.setText)
+
+    def startdl(self):
+        self.text.emit("")
+        t1 = threading.Thread(target = self.getdata)
+        t1.start()
 
     def getdata(self):
         global URL
-        self.progressBar.setValue(1)
+        self.progress.emit(1)
         URL = self.urlinp.text()
         if self.v480.isChecked():
             self.dler(URL, 480)
@@ -47,7 +65,7 @@ class app(QWidget):
     def getinfo(self, link, optA, optV = None):
         global VA, vT
         adict = youtube_dl.YoutubeDL(optA).extract_info(link, download=False)
-        aid = adict.get("id", None)
+        aid = adict.get("channel_id", None)
         aext = adict.get("ext", None)
         if optV != None:
             vdict = youtube_dl.YoutubeDL(optV).extract_info(link, download=False)
@@ -55,13 +73,13 @@ class app(QWidget):
             vext = vdict.get("ext", None)
             vtitle = vdict.get("title", None)
             adict = youtube_dl.YoutubeDL(optA).extract_info(link, download=False)
-            aid = adict.get("id", None)
+            aid = adict.get("channel_id", None)
             aext = adict.get("ext", None)
             VA = [str(vid + "." + vext), str(aid + "." + aext)]
-            vT = str(vtitle)
+            vT = remove(str(vtitle))
         else:
             VA = str(aid + "." + aext)
-            vT = str(adict.get("title"))
+            vT = remove(str(adict.get("title")))
 
         
 
@@ -72,33 +90,35 @@ class app(QWidget):
         else: 
             self.optionsV = {"format" : "bestvideo", "outtmpl" : r"%(id)s.%(ext)s"}
         
-        self.optionsA = {"format" : "bestaudio", "outtmpl" : r"%(id)s.%(ext)s"}
+        self.optionsA = {"format" : "bestaudio", "outtmpl" : r"%(channel_id)s.%(ext)s"}
         
         self.ytdl(vlink, self.optionsA, self.optionsV)
-        self.progressBar.setValue(2)
+        self.progress.emit(2)
 
         self.getinfo(vlink, self.optionsA, self.optionsV)
-        self.progressBar.setValue(3)
-
+        self.progress.emit(3)
         v = ffmpeg.input(VA[0])
         a = ffmpeg.input(VA[1])
+
+        print(VA[0])
+        print(VA[1])
 
         ff = ffmpeg.output(v, a, "{}.mp4".format(outpath + vT), vcodec='copy', acodec='aac')
         ff = ffmpeg.overwrite_output(ff)
         ff.run(cmd = str(wdir + "ffmpeg.exe"))
         os.remove(VA[0])
         os.remove(VA[1])
-        self.information.setText("{}.mp4 has been saved in the location:\n{}.mp4".format(vT, outpath + vT))
-        self.progressBar.setValue(4)
+        self.text.emit("{}.mp4 has been saved in the location:\n{}.mp4".format(vT, outpath + vT))
+        self.progress.emit(4)
 
     def mp3dl(self, vlink):
-        self.optionsA = {"format" : "bestaudio", "outtmpl" : r"%(id)s.%(ext)s"}
+        self.optionsA = {"format" : "bestaudio", "outtmpl" : r"%(channel_id)s.%(ext)s"}
         
         self.ytdl(vlink, self.optionsA)
-        self.progressBar.setValue(2)
+        self.progress.emit(2)
 
         self.getinfo(vlink, self.optionsA)
-        self.progressBar.setValue(3)
+        self.progress.emit(3)
 
         a = ffmpeg.input(VA)
 
@@ -106,12 +126,12 @@ class app(QWidget):
         ff = ffmpeg.overwrite_output(ff)
         ff.run(cmd = str(wdir + "ffmpeg.exe"))
         os.remove(VA)
-        self.information.setText("{}.mp3 has been saved in the location:\n{}.mp3".format(vT, outpath + vT))
-        self.progressBar.setValue(4)
+        self.text.emit("{}.mp3 has been saved in the location:\n{}.mp3".format(vT, outpath + vT))
+        self.progress.emit(4)
 
 gui = QApplication(sys.argv)
 
 window = app()
 window.show()
 
-gui.exec()
+threading.Thread(target = gui.exec())
